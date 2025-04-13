dc4e26eb7a4c401077fcd0b08ffbbe6761c10704
baisefuel <s.bril03@gmail.com>
2025-04-14 01:34:42 +0500

    clean files

--- clear_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-db = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'recipes_ingredients.csv'))
-
-db.drop(['description', 'tags', 'id'], axis=1, inplace=True)
-
-db.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'upd_recipes_ingredients.csv'))
