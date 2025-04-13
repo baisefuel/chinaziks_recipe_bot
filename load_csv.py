d555a9e0b9b34a4404b9271940f49d3535472da1
baisefuel <s.bril03@gmail.com>
2025-04-13 17:21:37 +0500

    load connection and create environments

--- load_csv.py
+++ load_csv.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user = os.getenv("DB_USER"),
+    host = os.getenv("DB_HOST"),
+    password = os.getenv("DB_PASSWORD")
+)
+cursor = connection.cursor()
 
 ds = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'recipes_ingredients.csv'))
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-
+from dotenv import load_dotenv
 import telegram
 
+load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TG_TOKEN')
 CHAT_ID = os.getenv('TG_CHAT_ID')
