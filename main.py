205c99681d8b4fd2931524a3f182d83a16e310c6
baisefuel <s.bril03@gmail.com>
2025-04-20 15:42:57 +0500

    add search buttons

--- main.py
+++ main.py
@@ -13,18 +13,33 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text(
-        "Добро пожаловать в Treat's Searcher!\n"
+        "Добро пожаловать в Treat's Searcher! 👨🏾‍🦯\n"
         "Бот для интеллектуального поиска лучших рецептов 🥵\n"
-        "Что ты хочешь сделать?😊",
+        "Что ты хочешь сделать? 😊",
         reply_markup=reply_markup
     )
 
 async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
+    keyboard = [
+        [InlineKeyboardButton("Найти по названию блюда 🍽", callback_data='search_by_name')],
+        [InlineKeyboardButton("Найти по ингредиенту 🍗", callback_data='search_by_igredients')],
+        [InlineKeyboardButton("Назад ⏪", callback_data='back')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
     if query.data == 'search':
-        await query.edit_message_text(text="Напиши название блюда или ингредиент 🍽")
+        await query.edit_message_text(text="Как ты хочешь найти рецепт?", reply_markup=reply_markup)
+    elif query.data == 'back':
+        keyboard = [
+            [InlineKeyboardButton("Найти рецепт🔍", callback_data='search')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text("Добро пожаловать в Treat's Searcher! 👨🏾‍🦯\n"
+        "Бот для интеллектуального поиска лучших рецептов 🥵\n"
+        "Что ты хочешь сделать? 😊", reply_markup=reply_markup)
+
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
