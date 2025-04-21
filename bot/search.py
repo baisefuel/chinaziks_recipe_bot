44b4a403c29dec9e2df4491c7737e2a1d2a4465b
baisefuel <s.bril03@gmail.com>
2025-04-21 23:48:01 +0500

    add search system 1.0

--- bot/search.py
+++ bot/search.py
@@ -9,9 +9,6 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-
-#asyncio.run(TranslateText())
-
 async def translate_to_en(text: str):
     return GoogleTranslator(source='ru', target='en').translate(text)
 
